a458c9a7f5f99fcd1e783b56a4f1cba1b144ebcf
grigoriy.yashin <grigoriy.yashin.02@mail.ru>
2025-05-31 14:32:56 +0300

    Add IMicrophoneTranscriber interface for audio transcription

---
+++ Sources/Transcribers/IMicrophoneTranscriber.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+using NAudio.Wave;
+using Whisper.net;
+using System.Text;
+
+namespace WhisperCLI.Transcribers
+{
+    public interface IMicrophoneTranscriber
+    {
+        public abstract Task<FileInfo> TranscribeAudioAsync(WhisperProcessor processor, Func<bool> stopRecording, CancellationToken token);
+
+    };
+}
